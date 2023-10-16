db411c52ea6c9608958877886d3474aee945af3f
hebersolano <74209829+hebersolano@users.noreply.github.com>
2023-10-15 19:23:28 -0600

    first-commmit

---
+++ 23-08-04-File loading/script.js
@@ -0,0 +1,5 @@
+let cloudBox = document.querySelector(".cloud-box");
+let btnCloud = document.querySelector(".btn-cloud");
+btnCloud.addEventListener("click", function () {
+  cloudBox.classList.toggle("opened");
+});
